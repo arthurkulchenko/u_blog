ddcb2bd9f4a211eb5eb3e1ad839d354237fc0fcc
Megalapa <megalapa@MBA.local>
2017-09-22 17:56:55 +0300

    + model tests

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_many :ip_list
+  has_many :ip_lists
   has_many :marks
 end
--- spec/models/mark_spec.rb
+++ spec/models/mark_spec.rb
@@ -1,4 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Mark, type: :model do
+  it { should belong_to(:user) }
+  it { should belong_to(:post) }
 end
